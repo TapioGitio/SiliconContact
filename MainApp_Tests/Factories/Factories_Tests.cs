ba8d915007e63dd7aa439394c59d460d44688784
Janne <tapioninetytwo@live.se>
2024-12-09 11:35:35 +0100

    Made some unit tests

---
+++ MainApp_Tests/Factories/Factories_Tests.cs
@@ -0,0 +1,32 @@
+﻿using Business.Factories;
+using Business.Models;
+
+namespace MainApp_Tests.Factories;
+
+public class Factories_Tests
+{
+    [Fact]
+    public void ContactRegistrationForm_ShouldCreateAnInstanceOf_ContactRegistrationForm()
+    {
+        var result = ContactFactory.Create();
+        Assert.IsType<ContactRegistrationForm>(result);
+    }
+
+    [Fact]
+    public void ContactEntity_ShouldCreateAnInstanceOf_ContactEntity()
+    {
+        ContactRegistrationForm contactRegistrationForm = new();
+        var result = ContactFactory.Create(contactRegistrationForm);
+
+        Assert.IsType<ContactEntity>(result);
+    }
+
+    [Fact]
+    public void Contact_ShouldCreateAnInstanceOf_Contact()
+    {
+        ContactEntity contactEntity = new();
+        var result = ContactFactory.Create(contactEntity);
+
+        Assert.IsType<Contact>(result);
+    }
+}
---
+++ MainApp_Tests/Handlers/Handlers_Tests.cs
@@ -0,0 +1,15 @@
+﻿using Business.Handlers;
+
+namespace MainApp_Tests.Handlers;
+
+public class Handlers_Tests
+{
+    [Fact]
+    public void IdGenerator_ShouldReturnATypeOfGuid()
+    {
+        var result = IdGenerator.GenerateID();
+
+        Assert.True(Guid.TryParse(result, out _));
+        Assert.False(string.IsNullOrEmpty(result));
+    }
+}
