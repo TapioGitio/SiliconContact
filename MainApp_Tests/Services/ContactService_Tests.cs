2a31575e80b7adce8bb9e04111ff02eedd652697
Janne <tapioninetytwo@live.se>
2024-12-11 13:57:00 +0100

    ongoing problems in mocking tests, saving to come back later

---
+++ MainApp_Tests/Services/ContactService_Tests.cs
@@ -0,0 +1,30 @@
+﻿using Business.Interfaces;
+using Business.Models;
+using Business.Services;
+using Moq;
+
+namespace MainApp_Tests.Services;
+
+public class ContactService_Tests
+{
+    private readonly Mock<IContactService> _contactServiceMock;
+    private readonly IContactService _contactService;
+
+    public ContactService_Tests()
+    {
+        _contactServiceMock = new Mock<IContactService>();
+        _contactService = new ContactService(_contactServiceMock.Object);
+    }
+
+    [Fact]
+    public void AddContact_ShouldAddContactToList()
+    {
+        var contactRegFrom = new ContactRegistrationForm();
+
+        _storageServiceMock
+            .Setup(ss => ss.SaveContactsToStorage(It.IsAny<string>()))
+            .Returns(true);
+
+        var result = _contactService.Add(contactRegFrom);
+    }
+}
--- MainApp_Tests/Services/StorageService_Tests.cs
+++ MainApp_Tests/Services/StorageService_Tests.cs
@@ -1,6 +1,30 @@
-﻿namespace MainApp_Tests.Services;
+﻿using Business.Interfaces;
+using Business.Models;
+using Business.Services;
+using Moq;
+
+namespace MainApp_Tests.Services;
 
 public class StorageService_Tests
 {
+    private readonly Mock<IStorageService> _storageServiceMock;
+    private readonly IContactService _contactService;
 
+    public StorageService_Tests()
+    {
+        _storageServiceMock = new Mock<IStorageService>();
+        _contactService = new ContactService(_storageServiceMock.Object);
+    }
+
+    [Fact]
+    public void AddContact_ShouldAddContactToStorage()
+    {
+        var contactEntity = new ContactEntity();
+
+        _storageServiceMock
+            .Setup(ss => ss.SaveContactsToStorage(contactEntity))
+            .Returns(true);
+
+        var result = _contactService.Add(contactEntity);
+    }
 }
