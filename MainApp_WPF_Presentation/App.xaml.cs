33accfa607fd6e0c76cfb89fea7c103ac7811c61
Janne <tapioninetytwo@live.se>
2024-12-17 11:14:55 +0100

    Added a createdefaultbuilder to app.xaml not done yet

--- MainApp_WPF_Presentation/App.xaml.cs
+++ MainApp_WPF_Presentation/App.xaml.cs
@@ -1,14 +1,24 @@
-﻿using System.Configuration;
+﻿using Business.Interfaces;
+using Business.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
 namespace MainApp_WPF_Presentation
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+
     public partial class App : Application
     {
+        IHost host = Host.CreateDefaultBuilder()
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<IContactService, ContactService>();
+        services.AddSingleton<IStorageService, StorageService>();
+    })
+    .Build();
+
     }
 
 }
