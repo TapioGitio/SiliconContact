e81c3e42de43eadc46170a98aecfb2598a110efa
Janne <tapioninetytwo@live.se>
2024-12-18 16:37:31 +0100

    Tried to make menus relays shorter with <T> but did not work. Alot of code repeated.

--- MainApp_WPF_Presentation/MainWindow.xaml.cs
+++ MainApp_WPF_Presentation/MainWindow.xaml.cs
@@ -1,22 +1,13 @@
 ﻿using MainApp_WPF_Presentation.ViewModels;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MainApp_WPF_Presentation;
 
 public partial class MainWindow : Window
 {
-    public MainWindow(MainViewModel viewModel)
+    public MainWindow(MainViewModel mainViewModel)
     {
         InitializeComponent();
-        DataContext = viewModel;
+        DataContext = mainViewModel;
     }
 }
